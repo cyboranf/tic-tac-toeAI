01b7c8040d0bd45156b327f47df68be2ed526ff6
cyboranf <cyboranf@gmail.com>
2023-03-21 21:13:09 +0100

    Project structure -changed

--- src/main/java/com/example/TicTacToe/TicTacToeApplication.java
+++ src/main/java/com/example/TicTacToe/TicTacToeApplication.java
@@ -2,10 +2,11 @@ package com.example.TicTacToe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class TicTacToeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TicTacToeApplication.class, args);
 	}
